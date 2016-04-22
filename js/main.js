93721d59ce78e26c78b79fcdf993a418c9754f58
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-04-22 10:45:56 -0400

    Added a waypoint to the bottom.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
-var $mam = $('.mam');
-var $ple = $('.ple');
-var $din = $('.din');
-var $pte = $('.pte');
+var $time = $('.time');
 
-$mam.waypoint(function() {
-  $mam.addClass('js-mam-animate')
-}, { offset: '50%' });
-
-$ple.waypoint(function() {
-  $ple.addClass('js-ple-animate')
-}, { offset: '50%' });
-
-$din.waypoint(function() {
-  $din.addClass('js-din-animate')
-}, { offset: '50%' });
-
-$pte.waypoint(function() {
-  $pte.addClass('js-pte-animate')
-}, { offset: '70%' });
+$time.waypoint(function() {
+  $time.addClass('js-time-animate')
+}, { offset: '85%' });
