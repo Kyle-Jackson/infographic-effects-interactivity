f8db19f2b160b912f32fe33cabac08c32def9998
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-04-22 11:08:36 -0400

    Added waypoint for boxes.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var $time = $('.time');
+var $girl = $('.girl');
 
 $time.waypoint(function() {
   $time.addClass('js-time-animate')
 }, { offset: '85%' });
+
+$girl.waypoint(function() {
+  $girl.addClass('js-girl-animate')
+}, { offset: '-3%' });
