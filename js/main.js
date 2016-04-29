5a60298130fe01367fe688183ee33d2d49922585
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-04-28 21:03:35 -0400

    Adjusted the position of the transition of the cookies.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ var $time = $('.time');
 var $girl = $('.girl');
 
 $time.waypoint(function() {
-  $time.addClass('js-time-animate')
+  $time.addClass('js-time-animate');
 }, { offset: '85%' });
 
 $girl.waypoint(function() {
-  $girl.addClass('js-girl-animate')
+  $girl.addClass('js-girl-animate');
 }, { offset: '0%' });
