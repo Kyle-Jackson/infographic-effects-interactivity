9f9d1f66e1512a2cfbdb93d5a358a49516b033ea
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-04-28 21:31:04 -0400

    Fixed pixel unit issue.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var $time = $('.time');
 var $girl = $('.girl');
 
-$time.waypoint(function() {
+$time.waypoint(function () {
   $time.addClass('js-time-animate');
-}, { offset: '85%' });
+}, {offset: '85%'});
 
-$girl.waypoint(function() {
+$girl.waypoint(function () {
   $girl.addClass('js-girl-animate');
-}, { offset: '0%' });
+}, {offset: '0%'});
